0bc2a8c51824f3ce2bd6b7243e650805f53c325b
7angJung <peterj0117@naver.com>
2025-11-28 03:19:03 +0900

    feat: register Spring beans using @Configuration and @Bean

---
+++ src/main/java/hello/hello/SpringConfig.java
@@ -0,0 +1,21 @@
+package hello.hello;
+
+import hello.hello.repository.MemberRepository;
+import hello.hello.repository.MemoryMemberRepository;
+import hello.hello.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
